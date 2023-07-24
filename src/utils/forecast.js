d47d2bbd687d8e8601e28461705e509a8861d995
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-24 14:41:07 -0400

    Building a JSON HTTP Endpoint

---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=325d5f8462a2e853d9ba2d344336725f&query=' +
+    latitude +
+    ',' +
+    longitude +
+    '&units=f';
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service! ', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          '. It is currently ' +
+          body.current.temperature +
+          ' degrees out. It feels like ' +
+          body.current.feelslike +
+          ' degrees out.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
